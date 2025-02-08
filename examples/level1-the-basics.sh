4ee9ea88aa8e3cf5e911c4c9c6c13e33233b168a
fnaeem-sketch <fnaeem2@gmail.com>
2025-02-08 16:36:17 +0000

    add level 1 solution

---
+++ examples/level1-the-basics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Level 1: The Basics
+
+# Mission: Create a directory named Arena and then inside it, 
+# create three files: warrior.txt, mage.txt, and archer.txt. 
+# List the contents of the Arena directory.
+
+mkdir arena
+cd arena
+touch warrior.txt mage.txt archer.txt
+ls
