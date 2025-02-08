f7e9f83a0865265663668df0014f3ea149542523
fnaeem-sketch <fnaeem2@gmail.com>
2025-02-08 17:00:38 +0000

    add level 4

---
+++ examples/level4-file-manipulation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Level 4: File Manipulation
+# Mission: Create a script that copies all .txt files from the
+# Arena directory to a new directory called Backup.
+
+mkdir -p backup
+cp arena/*.txt backup/
